4f350ebdff95ab3cce08d8efc446576b05884a25
中井 <ffz5mecx@gmail.com>
2024-03-30 03:22:25 +0900

    fetch test

--- src/App.jsx
+++ src/App.jsx
@@ -13,54 +13,44 @@ function App() {
     initializeLIFF();
   }, []);
 
-  const initializeLIFF = () => {
-    liff
-      .init({
+  const initializeLIFF = async () => {
+    try {
+      await liff.init({
         liffId: import.meta.env.VITE_LIFF_ID // ローカルではこっち
-      })
-      .then(() => {
-        alert("LIFF init succeeded.");
-        // LIFF初期化後、URLからshop_idを取得
-        const params = new URLSearchParams(window.location.search);
-        setShopId(params.get('shop_id'));
-        alert(shopId);
-        const responseData = fetchShopInfo(shopId); // fetchShopInfoを呼び出し
-
-        setShopInfo(responseData);
-      })
-      .catch((error) => {
-        alert("LIFF init failed.");
-        setError(`${error}`);
-        alert(error);
       });
+      alert("LIFF init succeeded.");
+      // LIFF初期化後、URLからshop_idを取得
+      const params = new URLSearchParams(window.location.search);
+      const shopId = params.get('shop_id');
+      setShopId(shopId);
+      alert(shopId);
+      const responseData = await fetchShopInfo(shopId); // fetchShopInfoを呼び出し
+      setShopInfo(responseData);
+    } catch (error) {
+      alert("LIFF init failed.");
+      setError(`${error}`);
+      alert(error);
+    }
   };
   
-  function fetchShopInfo(shopId) {
-    alert(shopId)
-    const url = "https://62da9f8e44ec.ngrok.app/shop_info?shop_id=" + shopId;
-    // const url = "https://62da9f8e44ec.ngrok.app/shop_info/" + shopId;
-    axios.get(url, { headers: {
-      "ngrok-skip-browser-warning": "69420"
-    }})
-    .then(response => {
-      alert(response.name)
-    });
-  }
-    
-//     try {
-//         const response = 
-
-//         if (!response.ok) {
-//             throw new Error('Network response was not ok');
-//         }
-
-//         const data = response.json();
-//         return data;
-//     } catch (error) {
-//         console.error('Error fetching shop info:', error);
-//         throw error;
-//     }
-// };
+  const fetchShopInfo = async (shopId) => {
+    try {
+      const url = "https://62da9f8e44ec.ngrok.app/shop_info?shop_id=" + shopId;
+      const response = await axios.get(url, {
+        headers: {
+          "ngrok-skip-browser-warning": "69420"
+        }
+      });
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = response.data;
+      return data;
+    } catch (error) {
+      console.error('Error fetching shop info:', error);
+      throw error;
+    }
+  };
 
   return (
     <>
