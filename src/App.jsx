ee738d2ad8db55805995ae393fb4e4821d560071
中井 <ffz5mecx@gmail.com>
2024-03-30 05:06:13 +0900

    fetch test

--- src/App.jsx
+++ src/App.jsx
@@ -108,7 +108,7 @@ function App() {
       if (response.status !== 200) {
         throw new Error('Network response was not ok');
       }
-      alert(response)
+      alert("reesponse.date: " + response.date)
       return response;
 
     } catch (error) {
