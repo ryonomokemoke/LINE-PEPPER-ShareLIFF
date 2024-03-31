5d76a869c190df0363d9f1688d6688d0595217f4
中井 <ffz5mecx@gmail.com>
2024-03-31 09:57:58 +0900

    fetch test

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ function App() {
   const createCarouselMessage = async (response) => {
     const responseJson = JSON.parse(response) // 文字列できているresponseをjson形式に
      // カルーセルに表示する 店舗へのアクセス、レビュー、レビュー数 のメッセージ
-    const shopOverview = `${responseJson.access}\n${responseJson.review_score}\n${responseJson.review_quantity}\n`
+    const shopOverview = `${responseJson.access}\n☆${responseJson.review_score}\n${responseJson.review_quantity}件のレビューの総評\n`
     alert(shopOverview);
     // カルーセルメッセージの内容を設定
     const carouselMessage = {
